9d3c2f3ef7bacd7391000a0ea7016dbef97781bd
Francis Tayag <zyenapz@gmail.com>
2025-02-24 09:22:04 +0800

    Allow program to create and add new books

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-alert("Hello World");
+const myLibrary = [];
+
+function Book(title, author, pages, hasBeenRead) {
+    this.title = title; 
+    this.author = author;
+    this.pages = pages;
+    this.hasBeenRead = hasBeenRead;
+}
+
+function addBookToLibrary(title, author, pages, hasBeenRead) {
+    book = new Book(title, author, pages, hasBeenRead);
+    myLibrary.push(book);
+}
+
+addBookToLibrary("The Martian", "Andy Weir", 320, false);
+addBookToLibrary("Do Androids Dream of Electric Sheep?", "Philip K. Dick", 269, false);
+addBookToLibrary("Masters of Doom", "David Kushner", 291, true);
+
+console.log(myLibrary);
