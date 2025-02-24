f09fb1fd261cd837bdcc1f4f038f9426f14bb960
Francis Tayag <zyenapz@gmail.com>
2025-02-24 11:06:22 +0800

    Switch to auto id system

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const ACTION_DELETE = "delete";
 const ACTION_TOGGLE = "toggle";
-const myLibrary = [];
+let auto_id = 0;
+const myLibrary = {};
 
 function Book(title, author, pages, hasBeenRead) {
     this.title = title; 
@@ -11,68 +12,87 @@ function Book(title, author, pages, hasBeenRead) {
 
 function addBookToLibrary(title, author, pages, hasBeenRead) {
     const book = new Book(title, author, pages, hasBeenRead);
-    myLibrary.push(book);
+    myLibrary[++auto_id] = book;
+    return book;
+}
+
+function addBookToDisplay(book, key=0) {
+    const table = document.querySelector("#library");
+    const row = document.createElement("div");
+    const para = document.createElement("p");
+    const title = document.createElement("span");
+    const author = document.createElement("span");
+    const pages = document.createElement("span");
+    const hasBeenRead = document.createElement("span");
+    const buttonToggle = document.createElement("button");
+    const buttonDelete = document.createElement("button");
+    
+    buttonToggle.textContent = "Toggle Read";
+    buttonDelete.textContent = "Delete";
+    buttonToggle.dataset.action = ACTION_TOGGLE;
+    buttonDelete.dataset.action = ACTION_DELETE;
+
+    title.textContent = book.title;
+    author.textContent = book.author;
+    pages.textContent = book.pages;
+    hasBeenRead.textContent = book.hasBeenRead ? "Yes" : "No";
+
+    para.append(title);
+    para.append(author);
+    para.append(pages);
+    para.append(hasBeenRead);
+
+    row.append(para);
+    row.append(buttonToggle);
+    row.append(buttonDelete);
+    row.dataset.key = key
+
+    row.addEventListener("click", (e) => {
+        const container = row.parentNode; 
+        const bookKey = row.dataset.key;
+        const action = e.target.dataset.action;
+        switch(action) {
+            case ACTION_DELETE:
+                deleteBook(bookKey);
+                container.removeChild(row);
+                break;
+            case ACTION_TOGGLE:
+                book.hasBeenRead = book.hasBeenRead ? false : true;
+                hasBeenRead.textContent = book.hasBeenRead ? "Yes" : "No";
+                break;
+        }
+    });
+
+    table.append(row);
 }
 
 function displayBooks() {
-    const table = document.querySelector("#library")
-    for(const book of myLibrary) {
-        const row = document.createElement("div");
-        const para = document.createElement("p");
-        const title = document.createElement("span");
-        const author = document.createElement("span");
-        const pages = document.createElement("span");
-        const hasBeenRead = document.createElement("span");
-        const buttonToggle = document.createElement("button");
-        const buttonDelete = document.createElement("button");
-        
-        buttonToggle.textContent = "Toggle Read";
-        buttonDelete.textContent = "Delete";
-        buttonToggle.dataset.action = ACTION_TOGGLE;
-        buttonDelete.dataset.action = ACTION_DELETE;
-
-        title.textContent = book.title;
-        author.textContent = book.author;
-        pages.textContent = book.pages;
-        hasBeenRead.textContent = book.hasBeenRead ? "Yes" : "No";
-
-        para.append(title);
-        para.append(author);
-        para.append(pages);
-        para.append(hasBeenRead);
-
-        row.append(para);
-        row.append(buttonToggle);
-        row.append(buttonDelete);
-        row.dataset.index = myLibrary.indexOf(book);
-        row.addEventListener("click", (e) => {
-            const container = row.parentNode; 
-            const bookIndex = row.dataset.index;
-            const action = e.target.dataset.action;
-            switch(action) {
-                case ACTION_DELETE:
-                    deleteBook(bookIndex);
-                    container.removeChild(row);
-                    break;
-                case ACTION_TOGGLE:
-                    book.hasBeenRead = book.hasBeenRead ? false : true;
-                    hasBeenRead.textContent = book.hasBeenRead ? "Yes" : "No";
-                    break;
-            }
-        })
-
-        table.appendChild(row);
+    for(const key in myLibrary) {
+        book = myLibrary[key];
+        console.log(key);
+        addBookToDisplay(book, key);
     }
     
 }
 
-function deleteBook(index) {
-    myLibrary.splice(index, 1);
+function deleteBook(key) {
+    delete myLibrary[key];
 }
-
 
 addBookToLibrary("The Martian", "Andy Weir", 320, false);
 addBookToLibrary("Do Androids Dream of Electric Sheep?", "Philip K. Dick", 269, false);
 addBookToLibrary("Masters of Doom", "David Kushner", 291, true);
-
 displayBooks();
+
+const buttonAddBook = document.querySelector("button[type=submit]");
+buttonAddBook.addEventListener("click", (e) =>
+{
+    const title = document.querySelector("#title").value;
+    const author = document.querySelector("#author").value;
+    const pages = document.querySelector("#pages").value;
+    const hasBeenRead = document.querySelector("input[name=book_read]:checked").value;
+
+    const book = addBookToLibrary(title, author, pages, Boolean(hasBeenRead));
+    addBookToDisplay(book, auto_id);
+    e.preventDefault();
+});
