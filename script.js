35679c941d1264db040a56251f8b53a00d2cf014
Francis Tayag <zyenapz@gmail.com>
2025-03-03 09:33:43 +0800

    Group DOM handling to a class

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
 "use strict"
 
-const ACTION_DELETE = "delete";
-const ACTION_TOGGLE = "toggle";
-const MATERIAL_CLASS = "material-symbols-outlined";
-const TXT_SECONDARY_CLASS = "txt-secondary";
-const ICON_READ = "check_circle";
-const ICON_NOTREAD = "radio_button_unchecked";
-const ICON_DELETE = "delete";
-const TXT_READ = "Done Reading";
-const TXT_NOTREAD = "Not Read";
-
 
 
 /* --- MODEL and CRUD --- */
@@ -42,96 +32,108 @@ class Book {
     }
 } 
 
-function toggleBook(book, htmlRead, htmlToggle) {
-    book.hasBeenRead = book.hasBeenRead ? false : true;
-    htmlRead.textContent = book.hasBeenRead ? TXT_READ : TXT_NOTREAD;
-    htmlToggle.innerText = book.hasBeenRead ? ICON_READ : ICON_NOTREAD;
-}
+class DOMHandler {
+    static ACTION_DELETE = "delete";
+    static ACTION_TOGGLE = "toggle";
+    static MATERIAL_CLASS = "material-symbols-outlined";
+    static TXT_SECONDARY_CLASS = "txt-secondary";
+    static ICON_READ = "check_circle";
+    static ICON_NOTREAD = "radio_button_unchecked";
+    static ICON_DELETE = "delete";
+    static TXT_READ = "Done Reading";
+    static TXT_NOTREAD = "Not Read";
 
-function addBookToDisplay(book, key=0) {
-    // -- General
-    const library = document.querySelector("#library");
-    const card = document.createElement("div");
-    const color = pickColor();
+    static toggleBook(book, htmlRead, htmlToggle) {
+        book.hasBeenRead = book.hasBeenRead ? false : true;
+        htmlRead.textContent = book.hasBeenRead ? TXT_READ : TXT_NOTREAD;
+        htmlToggle.innerText = book.hasBeenRead ? ICON_READ : ICON_NOTREAD;
+    }
     
-    // -- Logo group
-    const logo = document.createElement("div");
-    logo.classList.add("logo", color);
-
-    // -- Content group
-    const content = document.createElement("div");
-    content.classList.add("content");
-    const title = document.createElement("h2");
-    const author = document.createElement("p");
-    const pages = document.createElement("p");
-
-    title.textContent = book.title;
-    author.textContent = book.author;
-    pages.textContent = `${book.pages} pages`;
-    author.classList.add(TXT_SECONDARY_CLASS);
-    pages.classList.add(TXT_SECONDARY_CLASS);
-
-    // -- Actions group
-    const actions = document.createElement("div");
-    actions.classList.add("actions");
-
-    const hasBeenRead = document.createElement("span");
-    hasBeenRead.textContent = book.hasBeenRead ? TXT_READ : TXT_NOTREAD;
-
-    const buttonToggle = document.createElement("button");
-    const buttonDelete = document.createElement("button");
-    const iconToggle = document.createElement("span");
-    const iconDelete = document.createElement("span");
-
-    iconToggle.innerText = book.hasBeenRead ? ICON_READ : ICON_NOTREAD;
-    iconToggle.classList.add(MATERIAL_CLASS, color);
-
-    iconDelete.innerText = ICON_DELETE;
-    iconDelete.classList.add(MATERIAL_CLASS, color);
+    static addBookToDisplay(book, key=0) {
+        // -- General
+        const library = document.querySelector("#library");
+        const card = document.createElement("div");
+        const color = pickColor();
+        
+        // -- Logo group
+        const logo = document.createElement("div");
+        logo.classList.add("logo", color);
     
-    buttonToggle.setAttribute("type", "button");
-    buttonToggle.classList.add("action");
-    buttonToggle.dataset.action = ACTION_TOGGLE;
-    buttonToggle.append(iconToggle, hasBeenRead);
-
-    buttonDelete.setAttribute("type", "button");
-    buttonDelete.classList.add("action");
-    buttonDelete.dataset.action = ACTION_DELETE;
-    buttonDelete.append(iconDelete);
-
-    // -- Assemble card
-    actions.append(buttonToggle, buttonDelete);
-    content.append(title, author, pages);
-    card.append(logo, content, actions);
-    card.classList.add("card");
-    card.dataset.key = key
-    card.addEventListener("click", (e) => {
-        const container = library; 
-        const bookKey = card.dataset.key;
-
-        const button = e.target.parentNode;
-        const action = button.dataset.action;
-
-        switch(action) {
-            case ACTION_DELETE:
-                deleteBook(bookKey);
-                container.removeChild(card);
-                break;
-            case ACTION_TOGGLE:
-                toggleBook(book, hasBeenRead, iconToggle);
-                break;
+        // -- Content group
+        const content = document.createElement("div");
+        content.classList.add("content");
+        const title = document.createElement("h2");
+        const author = document.createElement("p");
+        const pages = document.createElement("p");
+    
+        title.textContent = book.title;
+        author.textContent = book.author;
+        pages.textContent = `${book.pages} pages`;
+        author.classList.add(this.TXT_SECONDARY_CLASS);
+        pages.classList.add(this.TXT_SECONDARY_CLASS);
+    
+        // -- Actions group
+        const actions = document.createElement("div");
+        actions.classList.add("actions");
+    
+        const hasBeenRead = document.createElement("span");
+        hasBeenRead.textContent = book.hasBeenRead ? this.TXT_READ : this.TXT_NOTREAD;
+    
+        const buttonToggle = document.createElement("button");
+        const buttonDelete = document.createElement("button");
+        const iconToggle = document.createElement("span");
+        const iconDelete = document.createElement("span");
+    
+        iconToggle.innerText = book.hasBeenRead ? this.ICON_READ : this.ICON_NOTREAD;
+        iconToggle.classList.add(this.MATERIAL_CLASS, color);
+    
+        iconDelete.innerText = this.ICON_DELETE;
+        iconDelete.classList.add(this.MATERIAL_CLASS, color);
+        
+        buttonToggle.setAttribute("type", "button");
+        buttonToggle.classList.add("action");
+        buttonToggle.dataset.action = this.ACTION_TOGGLE;
+        buttonToggle.append(iconToggle, hasBeenRead);
+    
+        buttonDelete.setAttribute("type", "button");
+        buttonDelete.classList.add("action");
+        buttonDelete.dataset.action = this.ACTION_DELETE;
+        buttonDelete.append(iconDelete);
+    
+        // -- Assemble card
+        actions.append(buttonToggle, buttonDelete);
+        content.append(title, author, pages);
+        card.append(logo, content, actions);
+        card.classList.add("card");
+        card.dataset.key = key
+        card.addEventListener("click", (e) => {
+            const container = library; 
+            const bookKey = card.dataset.key;
+    
+            const button = e.target.parentNode;
+            const action = button.dataset.action;
+    
+            switch(action) {
+                case ACTION_DELETE:
+                    deleteBook(bookKey);
+                    container.removeChild(card);
+                    break;
+                case ACTION_TOGGLE:
+                    toggleBook(book, hasBeenRead, iconToggle);
+                    break;
+            }
+        });
+    
+        // -- Put it all together
+        library.append(card);
+    
+    }
+    
+    static displayBooks() {
+        for(const key in Library.myLibrary) {
+            const book = Library.myLibrary[key];
+            this.addBookToDisplay(book, key);
         }
-    });
-
-    // -- Put it all together
-    library.append(card);
-
-}
-
-function displayBooks() {
-    for(const key in Library.myLibrary) {
-        const book = Library.myLibrary[key];
-        addBookToDisplay(book, key);
     }
 }
 
@@ -180,4 +182,4 @@ function pickColor() {
 Library.addBook("The Martian", "Andy Weir", 320, false);
 Library.addBook("Do Androids Dream of Electric Sheep?", "Philip K. Dick", 269, false);
 Library.addBook("Masters of Doom", "David Kushner", 291, true);
-displayBooks();
+DOMHandler.displayBooks();
