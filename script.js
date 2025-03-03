b5385088bc68a4c3006f48d7a8f6b7fda38216a2
Francis Tayag <zyenapz@gmail.com>
2025-03-03 09:38:43 +0800

    Prettify the source code formatting

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 "use strict"
 
-
-
-/* --- MODEL and CRUD --- */
-
 class Library {
     static #auto_id = 0
     static myLibrary = {}
@@ -13,7 +9,7 @@ class Library {
         this.myLibrary[++this.#auto_id] = book;
         return book;
     }
-    
+
     static deleteBook(key) {
         delete this.myLibrary[key];
     }
@@ -25,12 +21,12 @@ class Library {
 
 class Book {
     constructor(title, author, pages, hasBeenRead) {
-        this.title = title; 
+        this.title = title;
         this.author = author;
         this.pages = pages;
         this.hasBeenRead = hasBeenRead;
     }
-} 
+}
 
 class DOMHandler {
     static ACTION_DELETE = "delete";
@@ -48,58 +44,58 @@ class DOMHandler {
         htmlRead.textContent = book.hasBeenRead ? this.TXT_READ : this.TXT_NOTREAD;
         htmlToggle.innerText = book.hasBeenRead ? this.ICON_READ : this.ICON_NOTREAD;
     }
-    
-    static addBookToDisplay(book, key=0) {
+
+    static addBookToDisplay(book, key = 0) {
         // -- General
         const library = document.querySelector("#library");
         const card = document.createElement("div");
         const color = Utils.pickColor();
-        
+
         // -- Logo group
         const logo = document.createElement("div");
         logo.classList.add("logo", color);
-    
+
         // -- Content group
         const content = document.createElement("div");
         content.classList.add("content");
         const title = document.createElement("h2");
         const author = document.createElement("p");
         const pages = document.createElement("p");
-    
+
         title.textContent = book.title;
         author.textContent = book.author;
         pages.textContent = `${book.pages} pages`;
         author.classList.add(this.TXT_SECONDARY_CLASS);
         pages.classList.add(this.TXT_SECONDARY_CLASS);
-    
+
         // -- Actions group
         const actions = document.createElement("div");
         actions.classList.add("actions");
-    
+
         const hasBeenRead = document.createElement("span");
         hasBeenRead.textContent = book.hasBeenRead ? this.TXT_READ : this.TXT_NOTREAD;
-    
+
         const buttonToggle = document.createElement("button");
         const buttonDelete = document.createElement("button");
         const iconToggle = document.createElement("span");
         const iconDelete = document.createElement("span");
-    
+
         iconToggle.innerText = book.hasBeenRead ? this.ICON_READ : this.ICON_NOTREAD;
         iconToggle.classList.add(this.MATERIAL_CLASS, color);
-    
+
         iconDelete.innerText = this.ICON_DELETE;
         iconDelete.classList.add(this.MATERIAL_CLASS, color);
-        
+
         buttonToggle.setAttribute("type", "button");
         buttonToggle.classList.add("action");
         buttonToggle.dataset.action = this.ACTION_TOGGLE;
         buttonToggle.append(iconToggle, hasBeenRead);
-    
+
         buttonDelete.setAttribute("type", "button");
         buttonDelete.classList.add("action");
         buttonDelete.dataset.action = this.ACTION_DELETE;
         buttonDelete.append(iconDelete);
-    
+
         // -- Assemble card
         actions.append(buttonToggle, buttonDelete);
         content.append(title, author, pages);
@@ -107,13 +103,13 @@ class DOMHandler {
         card.classList.add("card");
         card.dataset.key = key
         card.addEventListener("click", (e) => {
-            const container = library; 
+            const container = library;
             const bookKey = card.dataset.key;
-    
+
             const button = e.target.parentNode;
             const action = button.dataset.action;
-    
-            switch(action) {
+
+            switch (action) {
                 case this.ACTION_DELETE:
                     this.deleteBook(bookKey);
                     container.removeChild(card);
@@ -123,21 +119,19 @@ class DOMHandler {
                     break;
             }
         });
-    
+
         // -- Put it all together
         library.append(card);
-    
+
     }
-    
+
     static displayBooks() {
-        for(const key in Library.myLibrary) {
+        for (const key in Library.myLibrary) {
             const book = Library.myLibrary[key];
             this.addBookToDisplay(book, key);
         }
     }
 }
-
-/* --- Modal and Form --- */
 
 class Modal {
     constructor() {
@@ -153,8 +147,7 @@ class Modal {
         buttonCloseModal.addEventListener("click", (e) => {
             dialog.close();
         })
-        buttonAddBook.addEventListener("click", (e) =>
-        {
+        buttonAddBook.addEventListener("click", (e) => {
             let title = document.querySelector("#title");
             let author = document.querySelector("#author");
             let pages = document.querySelector("#pages");
@@ -177,7 +170,7 @@ class Modal {
 class Utils {
     static pickColor() {
         const COLORS = ["bg-purple", "bg-blue", "bg-green", "bg-kale", "bg-desert"];
-        return COLORS[Math.floor(Math.random() * COLORS.length)];    
+        return COLORS[Math.floor(Math.random() * COLORS.length)];
     }
 }
 
